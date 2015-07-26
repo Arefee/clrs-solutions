796f9a4d43a7cbc1329fa045ece3147d0d6b0544
Georgy Shopov <gsshopov@gmail.com>
2015-07-26 17:52:28 +0300

    Add C++ code and tests for exercise 2.1-3

---
+++ part-01/chapter-02/section-01/exercise-03/code.hpp
@@ -0,0 +1,15 @@
+template <typename InputIterator, typename T>
+InputIterator linear_search(InputIterator first,
+                            InputIterator last,
+                            const T& value)
+{
+    for (auto i = first; i != last; ++i)
+    {
+        if (*i == value)
+        {
+            return i;
+        }
+    }
+
+    return last;
+}
---
+++ part-01/chapter-02/section-01/exercise-03/test.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+
+#include "code.hpp"
+#include "../../../../testlib/testlib.hpp"
+
+using namespace std;
+
+TEST(empty_sequence)
+{
+    vector<int> sequence;
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 1) ==
+                sequence.cend());
+}
+
+TEST(single_element_sequence)
+{
+    vector<int> sequence {42};
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 42) ==
+                sequence.cbegin());
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 10) ==
+                sequence.cend());
+}
+
+TEST(find_first_element)
+{
+    vector<int> sequence {34, 23, 9, 345};
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 34) ==
+                sequence.cbegin());
+}
+
+TEST(find_last_element)
+{
+    vector<int> sequence {34, 23, 9, 345};
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 345) ==
+                sequence.cbegin() + 3);
+}
+
+TEST(find_inner_element)
+{
+    vector<int> sequence {78, 667, 203, 8, 1023, 238, 300};
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 8) ==
+                sequence.cbegin() + 3);
+
+    ASSERT_TRUE(linear_search(sequence.cbegin(), sequence.cend(), 238) ==
+                sequence.cbegin() + 5);
+}
+
+int main()
+{
+    BEGIN();
+
+    RUN_TEST(empty_sequence);
+    RUN_TEST(single_element_sequence);
+    RUN_TEST(find_first_element);
+    RUN_TEST(find_last_element);
+    RUN_TEST(find_inner_element);
+
+    END();
+}
